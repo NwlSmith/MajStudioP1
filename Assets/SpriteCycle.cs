26bab38ba55929db0bd73744a625b22f24aa2016
FINNJC <finnjcarney@gmail.com>
2020-11-17 16:31:30 -0500

    Slight changes to cockpit, implimented News updates, scanlines, buttons only pressable on decision

---
+++ Assets/SpriteCycle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCycle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SpriteRenderer[] srs;
+    public Sprite[] sprites;
+    int spriteCounter;
+    int timer;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(timer < 3)
+        {
+            timer++;
+            Debug.Log("Timer Go Up");
+        }
+        else
+        {
+            timer = 0;
+            if(spriteCounter > sprites.Length)
+            {
+                Debug.Log("Counter to 0");
+                spriteCounter = 0;
+            }
+            else
+            {
+                Debug.Log("Counter go up");
+                spriteCounter++;
+            }
+            
+            for (int i = 0; i < srs.Length; i++)
+            {
+                srs[i].sprite = sprites[spriteCounter];
+            }
+        }
+    }
+}
