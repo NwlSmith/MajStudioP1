928941e36cfd724ec33e2d3d9163c3ec2ba5e1ac
Nate Smith <me@nwlsmith.com>
2020-11-21 16:20:36 -0500

    oops

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+
+    private Vector3 initPosition;
+    public int shakeModifier = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPosition = transform.position;
+
+        StartCoroutine(ShakeEnum());
+    }
+
+    private IEnumerator ShakeEnum()
+    {
+        while (true)
+        {
+            transform.position = initPosition + new Vector3(Random.Range(-shakeModifier, shakeModifier), Random.Range(-shakeModifier, shakeModifier), Random.Range(-shakeModifier, shakeModifier));
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
