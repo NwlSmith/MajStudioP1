49a792367daefa266da05794c336d84be5009cca
FINNJC <finnjcarney@gmail.com>
2020-11-20 20:08:32 -0500

    Made the WIn and Lose scenes with ability to return to main scene. New PC, so hopefully nothing breaks

---
+++ Assets/Scripts/SpriteCycle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCycle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SpriteRenderer[] srs;
+    public Sprite[] sprites;
+    int spriteCounter;
+    int timer;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(timer < 3)
+        {
+            timer++;
+        }
+        else
+        {
+            timer = 0;
+            if(spriteCounter + 2 > sprites.Length)
+            {
+                spriteCounter = 0;
+            }
+            else
+            {
+                spriteCounter++;
+            }
+            
+            for (int i = 0; i < srs.Length; i++)
+            {
+                srs[i].sprite = sprites[spriteCounter];
+            }
+        }
+    }
+}
