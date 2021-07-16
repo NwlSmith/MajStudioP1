c5f700321adb199e1961a8d90c150b349e099da5
Nate Smith <me@nwlsmith.com>
2021-07-16 15:12:05 -0400

    Added VR and preliminary tentacle animation to Logo screen.

---
+++ Assets/Logo/LogoSequenceScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoSequenceScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _tentacle = null;
+    [SerializeField] private Light _spotlight1 = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Sequence()
+    {
+        
+        
+        
+        yield return null;
+        SceneManager.LoadScene("IntroScene");
+    }
+}
